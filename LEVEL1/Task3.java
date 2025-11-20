36eccf9332579636373d6525b20f4738485bdad3
Prasanna <prasannavenkatesh070606@gmail.com>
2025-11-20 13:50:12 +0530

    task3 added

--- LEVEL1/Task3.java
+++ LEVEL1/Task3.java
@@ -1,5 +1,29 @@
 package LEVEL1;
+import java.util.*;
 
 public class Task3 {
+    public double avarageGradeCalculator(int[] grades){
+        int sum=0;
+        for(int i:grades){
+            sum+=i;
+        }
+        return (double) sum/grades.length;
+
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Task3 obj =new Task3();
+        System.out.println("Enter No.of grades");
+        int N=sc.nextInt();
+        int[] grades=new int[N];
+        for(int i=0;i<N;i++){
+            System.out.println("Enter Grade"+(i+1)+":");
+            grades[i]=sc.nextInt();
+        }
+        double average=obj.avarageGradeCalculator(grades);
+        System.out.println("The Average grade is "+ average);
+        
+
+    }
     
 }
